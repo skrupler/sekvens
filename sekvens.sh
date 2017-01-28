5446fe800a688419c0f01881fc098a269ed41834
skrupler <61eefedcb8c1e879@protonmail.ch>
2017-01-28 16:39:05 +0100

    fixes and more fixes

--- sekvens.sh
+++ sekvens.sh
@@ -13,9 +13,9 @@
 
 helpmsg(){
 
-    echo -e "sekvens.sh"
+    #echo -e "sekvens.sh"
 	echo -ne "\n"
-	echo -e "Usage:"
+	echo -e "\tUsage"
 	echo -ne "\n"
 	echo -e "\t$0/path/to/save/location -n \"armin van buuren a state of trance\" -d 3"
 	echo -ne "\n"
@@ -24,19 +24,19 @@ helpmsg(){
 	echo -e "\t-d" "\t" "Digits per episode number (ie. 800 has 3)"
 	echo -e "\t-p" "\t" "Prints this message"
 	echo -e "\t-h" "\t" "Prints missing shows to a file (default: missing.log)"
-
+	echo -ne "\n"
 
 }
 
 regex="^(va|armin.van.buuren)+.*a.state.of.trance.*[0-9][0-9][0-9].*2016.*"
 
-ARCHIVE_PATH=$1
+#ARCHIVE_PATH=$1
 
-ARCHIVE_PATH=$(ls "${ARCHIVE_PATH}" | egrep -oi ${regex})
+#ARCHIVE_PATH=$(ls "${ARCHIVE_PATH}" | egrep -oi ${regex})
 
-TEST=$(echo -ne "$ARCHIVE_PATH" "\n"| egrep -o "(_|-)+[0-9]{3}(_|-)+" | tr -d '-' | tr -d '_' | sort -n)
-lower=$(echo -ne "$ARCHIVE_PATH" "\n"| egrep -o "(_|-)+[0-9]{3}(_|-)+" | tr -d '-' | tr -d '_' | sort -rnk3 | awk '!x[$2]++')
-upper=$(echo -ne "$ARCHIVE_PATH" "\n"| egrep -o "(_|-)+[0-9]{3}(_|-)+" | tr -d '-' | tr -d '_' | sort -nk3 | awk '!x[2]++')
+#TEST=$(echo -ne "$ARCHIVE_PATH" "\n"| egrep -o "(_|-)+[0-9]{3}(_|-)+" | tr -d '-' | tr -d '_' | sort -n)
+#lower=$(echo -ne "$ARCHIVE_PATH" "\n"| egrep -o "(_|-)+[0-9]{3}(_|-)+" | tr -d '-' | tr -d '_' | sort -rnk3 | awk '!x[$2]++')
+#upper=$(echo -ne "$ARCHIVE_PATH" "\n"| egrep -o "(_|-)+[0-9]{3}(_|-)+" | tr -d '-' | tr -d '_' | sort -nk3 | awk '!x[2]++')
 
 
 #echo -ne "$ARCHIVE_PATH"
@@ -49,41 +49,58 @@ upper=$(echo -ne "$ARCHIVE_PATH" "\n"| egrep -o "(_|-)+[0-9]{3}(_|-)+" | tr -d '
 
 #comm -13 <(echo -e "${TEST}" | sort | uniq) <(echo "${SEQ}")
 
-#echo $upper
-#echo $lower
 
-while getopts "t:n:d:ph" opt; do
-	case $opt in	
-	t)
-		target=$OPTARG
-		;;
-	n)
-		name=$OPTARG
-		;;
-	d)	
-		digits=$OPTARG
-		;;
-	p)
-		# if this is true, tonight we feast
-		if $OPT;then
-			print_to_file=true
-		else
-			print_to_file=false
-		fi
-		;;
-	h)
-		# prints help msg
-		helpmsg
-		exit 1
-		;;
-	\?)
-		echo "Invalid option. -$OPTARG" >&2
-		exit 1
-		;;
-	:)
-		echo "Option -$OPTARG requires an argument." >&2
-		exit 1
-		;;
-	esac
-done
+main(){
+
+	target=""
+	name=""
+	digits=""
+	print_to_file=false
+
+
+
+	while getopts "t:n:d:ph" opt; do
+		case $opt in	
+		t)
+			target=$OPTARG
+			;;
+		n)
+			name=$OPTARG
+			;;
+		d)	
+			digits=$OPTARG
+			;;
+		p)
+			# if this is true, tonight we feast
+			if $OPT;then
+				print_to_file=true
+				echo ${print_to_file}
+			else
+				print_to_file=false
+			fi
+			;;
+		h)
+			# prints help msg
+			helpmsg
+			exit 1
+			;;
+
+		\?)
+			echo "Invalid option. -$OPTARG" >&2
+			exit 1
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument." >&2
+			exit 1
+			;;
+		*)
+			helpmsg
+			exit 1
+			;;
+		esac
+	done
+}
+
+
+main "$@"
 
